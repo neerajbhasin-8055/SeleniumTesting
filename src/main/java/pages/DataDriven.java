51c4d9f9c788178fd37f39954af0bd1b8c495583
neerajbhasin-8055 <bassanneeraj@gmail.com>
2026-07-01 18:27:00 +0530

    data driven

--- src/main/java/pages/DataDriven.java
+++ src/main/java/pages/DataDriven.java
@@ -1,4 +1,47 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import utils.WaitUtils;
+
 public class DataDriven {
+    private WebDriver driver;
+    private WaitUtils wait;
+
+    public DataDriven(WebDriver driver, WaitUtils wait){
+        this.driver = driver;
+        this.wait = new WaitUtils(driver);
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(id="ddt-username")
+    private WebElement userName;
+
+    @FindBy(id="ddt-password")
+    private  WebElement password;
+
+    @FindBy(id ="ddt-env")
+    private WebElement selectElement;
+
+    @FindBy(xpath = "//button[text() = 'Login']")
+    private WebElement loginBtn;
+
+    public void enterUsername(String user){
+        wait.waitForElementToBeVisible(userName).sendKeys(user);
+    }
+
+    public void enterPassword(String pass){
+        wait.waitForElementToBeVisible(password).sendKeys(pass);
+    }
+
+    public void selectDropdown(String env){
+        Select sel = new Select(wait.waitForElementToBeVisible(selectElement));
+        sel.selectByVisibleText(env);
+    }
+    public void login(){
+        wait.waitForElementToBeClickable(loginBtn).click();
+    }
+
 }
